ee0f578cbb030af1bf61f0e95f70bd70c7af99f6
Suryakumar S <suryakumarcr07@gmail.com>
2025-06-20 08:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     logger.info("Hello from Lambda")
     logger.info("Hi Team")
     logger.info("Webhook changes updated")
-    logger.info("Github changes updated")
+    logger.info("Github changes")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from GitHub Actions Lambda!')
